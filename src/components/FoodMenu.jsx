8ed972f9b2df0802b4c321e5e284acc2038ce7d1
Kavyashah119 <kavyashah2929@gmail.com>
2022-04-06 10:22:45 +0530

    Initial commit

---
+++ src/components/FoodMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './FoodMenu.css'
+
+const FoodMenu = () => {
+    return (
+        <>
+            <div class="hero">
+                <div class="nav-bar">
+                    
+                    
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default FoodMenu
